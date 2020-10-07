bedf3e14e946cc437c65dd46636dae32273fe3aa
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2020-10-07 14:00:20 +0200

    Update api.js

--- src/features/movies/api.js
+++ src/features/movies/api.js
@@ -1,4 +1,5 @@
 export const getPopularMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_API_KEY}`);
 
   if (!response.ok) {
     new Error(response.statusText);
