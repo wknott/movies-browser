80d6067a27f9220b637fbf1c219d6d93a6b9edbd
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-07 13:54:50 +0200

    added and set up store,moviesSaga and moviesSlice , changed api.js fegetPopularMovies function

--- src/features/movies/api.js
+++ src/features/movies/api.js
@@ -1,9 +1,13 @@
 export const getPopularMovies = async () => {
-  const response = await fetch(`https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_API_KEY}`);
+  const response = await fetch(
+    `https://api.themoviedb.org/3/person/popular?api_key=0e9e647ea010d8b3410709d41b745d87`
+  );
 
   if (!response.ok) {
     new Error(response.statusText);
   }
 
-  return await response.json().results;
-}
+  const movies = await response.json();
+
+  return movies.results;
+};
---
+++ src/features/movies/moviesSaga.js
@@ -0,0 +1,22 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import { getPopularMovies } from "./api";
+import {
+  fetchPopularMovies,
+  fetchPopularMoviesError,
+  fetchPopularMoviesSuccess,
+} from "./moviesSlice";
+
+function* fetchPopularMoviesHandler() {
+  try {
+    const popularMovies = yield call(getPopularMovies);
+    yield console.log(popularMovies);
+    yield put(fetchPopularMoviesSuccess(popularMovies));
+  } catch (error) {
+    yield call(alert, "Coś poszło nie tak! Spróbuj ponownie później.");
+    yield put(fetchPopularMoviesError());
+  }
+}
+
+export function* watchFetchPopularMovies() {
+  yield takeLatest(fetchPopularMovies.type, fetchPopularMoviesHandler);
+}
