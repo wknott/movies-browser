4cf3d9f908f737a9c5f4a571ffb620b975ddaf5c
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-10-07 20:15:24 +0200

    function getPersonMovieCredits

--- src/features/people/Api.js
+++ src/features/people/Api.js
@@ -22,3 +22,16 @@ export const getPopularPeople = async () => {
     return personDetails;
   };
 
+  export const getPersonMovieCredits = async (personId) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/person/{personId}/movie_credits?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+  
+    if (!response.ok) {
+      new Error(response.statusText);
+    };
+  
+    const PersonMovieCredits = await response.json();
+  
+    return PersonMovieCredits;
+  }; 
