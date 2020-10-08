be03534e21306e2db16320886d7ca8f3e17930b9
Wojciech Knott <wojciechknott@gmail.com>
2020-10-08 11:46:53 +0200

    added getGenreName function which get paramteres genreId and genres and return name of genre

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGenres, selectGenres } from "../moviesSlice";
 
-export default () => (
-  <h1>Popular Movies</h1>
-);
+export default () => {
+  const dispatch = useDispatch();
+  const genres = useSelector(selectGenres);
+
+  useEffect(() => {
+    dispatch(fetchGenres());
+  }, []);
+
+  return (
+    <h1>Popular Movies</h1>
+  );
+};
