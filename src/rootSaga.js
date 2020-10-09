f17d10bcde2b2d9012f7c489e1f5b0aa9fee550b
Wojciech Knott <wojciechknott@gmail.com>
2020-10-09 10:33:35 +0200

    binded two sagas in rootSaga and applied to store rootSaga

---
+++ src/rootSaga.js
@@ -0,0 +1,10 @@
+import { all } from "redux-saga/effects";
+import { watchFetchPopularMovies } from "./features/movies/moviesSaga";
+import { watchFetchPopularPeople } from "./features/people/peopleSaga";
+
+export default function* rootSaga() {
+  yield all([
+    watchFetchPopularMovies(),
+    watchFetchPopularPeople(),
+  ]);
+}
