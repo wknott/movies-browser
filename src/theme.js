ec8333414de4414bf82f68ee0bd4f03bd9fbd733
Wojciech Knott <wojciechknott@gmail.com>
2020-10-09 13:52:19 +0200

    added silver color to theme

--- src/theme.js
+++ src/theme.js
@@ -2,14 +2,13 @@ export const theme = {
     color: {
         black: "#18181B",
         blue: "#0044CC",
+        silver: "#C4C4C4",
         lightBlue: "#D6E4FF",
         yellow: "#FCD420",
         darkerGrey: "#7E839A",
         grey: "#E4E6F0",
         mercury: "#E5E5E5",
         white: "#FFFFFF",
-
-       
     },
     breakpoint: {
         mobileMax: 767,
