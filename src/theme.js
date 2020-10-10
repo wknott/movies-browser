68d2cabb56b05519785338aa3c8e71fd1c78bbea
Wojciech Knott <wojciechknott@gmail.com>
2020-10-10 15:44:26 +0200

    added storm gray color to theme

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,7 @@ export const theme = {
         silver: "#C4C4C4",
         lightBlue: "#D6E4FF",
         yellow: "#FCD420",
+        stormGray: "#74788B",
         darkerGrey: "#7E839A",
         grey: "#E4E6F0",
         mercury: "#E5E5E5",
