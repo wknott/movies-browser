7f8c08ea442e7c2acd392363dee36c3a398e1d4d
Wojciech Knott <wojciechknott@gmail.com>
2020-10-12 23:46:21 +0200

    Updates

---
+++ precache-manifest.e077ae87a06945de3e23fe0c1c7a7c7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79329f4bf89530db1bb8cf7cf507f58",
+    "url": "/movies-browser/index.html"
+  },
+  {
+    "revision": "2a63b6563d8f419e9b6c",
+    "url": "/movies-browser/static/js/2.619be46c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-browser/static/js/2.619be46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f3148ca9c9aa2766c3",
+    "url": "/movies-browser/static/js/main.e60f9c22.chunk.js"
+  },
+  {
+    "revision": "b4c463465b3baf8f8203",
+    "url": "/movies-browser/static/js/runtime-main.8e42f4f8.js"
+  },
+  {
+    "revision": "345aaccb39008981495bc1e39f5bd793",
+    "url": "/movies-browser/static/media/Profile.345aaccb.svg"
+  },
+  {
+    "revision": "b489e61393b0ed1bb8e71ee82f0e4821",
+    "url": "/movies-browser/static/media/ratingStar.b489e613.svg"
+  }
+]);
