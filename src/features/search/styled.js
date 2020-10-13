be410f7ca825e4d628f122fe78856fe11adca370
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-10-13 14:42:30 +0200

    a little changes

--- src/features/search/styled.js
+++ src/features/search/styled.js
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
-export const Label = styled.label `
-width: 432px;
-height: 48px;
-border-radius: 33px;
-background-color: ${({ theme }) => theme.color.white};
-border: 1px solid ${({ theme }) => theme.color.grey};
-display: flex;
-align-items: center;
-margin: 23px 200px;
+export const Label = styled.label`
+  width: 432px;
+  height: 48px;
+  border-radius: 33px;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 1px solid ${({ theme }) => theme.color.grey};
+  display: flex;
+  align-items: center;
+  margin: 23px 200px;
 `;
 
-export const Icon = styled.img `
-margin: 0 15px;
-width: 20px;
-height: 20px;
-border: 1px solid ${({ theme }) => theme.color.darkerGrey};
+export const Icon = styled.img`
+  margin: 0 15px;
+  width: 20px;
+  height: 20px;
+  border: 1px solid ${({ theme }) => theme.color.darkerGrey};
 `;
 
-export const Input = styled.input `
-background-color: none;
-border: none;
-font-size: 16px;
-flex-grow: 0.8;
+export const Input = styled.input`
+  background-color: none;
+  border: none;
+  font-size: 16px;
+  flex-grow: 0.8;
 `;
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,31 +1,37 @@
 import React from "react";
 import Search from "../features/search";
 import camera from "../images/camera.svg";
-import { Wrapper } from "./styled";
+import {
+  Wrapper,
+  Container,
+  NavigationStyle,
+  NavigationItem,
+  NavigationList,
+  Title,
+  Link,
+  Icon,
+} from "./styled";
 
 const Navigation = () => {
-    return (
-<Container>
-    <Wrapper>
+  return (
+    <Container>
+      <Wrapper>
         <NavigationStyle>
-<Icon src={camera} />
-<Title>
-    Movies Browser
-</Title>
-<NavigationList>
-    <NavigationItem>
-        <Link >Movies</Link>
-    </NavigationItem>
-    <NavigationItem>
-        <Link >People</Link>
-    </NavigationItem>
-   
-</NavigationList>
+          <Icon src={camera} />
+          <Title>Movies Browser</Title>
+          <NavigationList>
+            <NavigationItem>
+              <Link>Movies</Link>
+            </NavigationItem>
+            <NavigationItem>
+              <Link>People</Link>
+            </NavigationItem>
+          </NavigationList>
         </NavigationStyle>
-        <Search/>
-    </Wrapper>
-</Container>
-    );
+        <Search />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navigation;
