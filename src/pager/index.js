bd8f1092091a7d49cd38a5308a5b9bf9141c79dc
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-14 13:50:46 +0200

    changed structure of pager

--- src/pager/index.js
+++ src/pager/index.js
@@ -1,36 +1,43 @@
 import React from "react"
 import Prev from "./images/Prev.svg"
+import DisabledPrev from "./images/DisabledPrev.svg"
 import Next from "./images/Next.svg"
-import { PagerButton, PagerNext, PagerPrev, PagerText,PagerInfo,PagerWrapper, Bold } from "./styled"
+import DisabledNext from "./images/DisabledNext.svg"
+import { PagerButton, PagerNext, PagerPrev, PagerText , PagerInfo , PagerWrapper , Bold } from "./styled"
 import {useDispatch, useSelector} from "react-redux";
 import { selectCurrentPage, incrementPage,decrementPage, selectAllPages, setPageToFirst, setPageToLast } from "../features/movies/moviesSlice";
+import {theme} from "../theme";
+
 export const Pager = () => {
 
     const currentPage = useSelector(selectCurrentPage);
     const allPages = useSelector(selectAllPages);
-    const mobileWidth = ({theme}) => theme.breakpoint.mobileMax;
+    const mobileWidth = theme.breakpoint.mobileMax;
+    const disablePrevious = currentPage === 1 ? true : false;
+    const disableNext = currentPage === allPages ? true : false;
     const dispatch = useDispatch();
 
     return (
         <PagerWrapper>
             <PagerButton
             onClick={() => {dispatch(setPageToFirst())}}
-            disabled={currentPage === 1 ? true : false}
+            disabled={disablePrevious}
             >
-                <PagerPrev  src={Prev}/>
+                <PagerPrev  src={ disablePrevious ? DisabledPrev :Prev}/>
                 {window.innerWidth > mobileWidth ? 
                 <PagerText>
                     First
                 </PagerText>
-                : <PagerPrev src={Prev}/>}
+                : <PagerPrev  src={ disablePrevious ? DisabledPrev :Prev}/>
+                }
                 
             </PagerButton>
 
             <PagerButton
             onClick={() => {dispatch(decrementPage())}}
-            disabled={currentPage === 1 ? true : false}
+            disabled={disablePrevious}
             >
-                <PagerPrev src={Prev}/>
+                <PagerPrev src={disablePrevious ? DisabledPrev :Prev}/>
                 {window.innerWidth > mobileWidth ? 
                 <PagerText>
                     Previous
@@ -45,26 +52,27 @@ export const Pager = () => {
 
             <PagerButton 
             onClick={() => {dispatch(incrementPage())}}
-            disabled={currentPage === allPages ? true : false}
+            disabled={disableNext}
             >
                 {window.innerWidth > mobileWidth ? 
                 <PagerText>
                    Next
                 </PagerText>
                 : <></>}
-                <PagerNext src={Next}/>
+                <PagerNext src={disableNext ? DisabledNext : Next}/>
             </PagerButton>
 
             <PagerButton
             onClick={() => {dispatch(setPageToLast())}}
-            disabled={currentPage === allPages ? true : false}
+            disabled={disableNext}
             >
                 {window.innerWidth > mobileWidth ? 
                 <PagerText>
                    Last
                 </PagerText>
-                : <PagerNext src={Next}/>}
-                <PagerNext src={Next}/>
+                : <PagerNext src={disableNext ? DisabledNext : Next}/>
+                }
+                <PagerNext src={disableNext ? DisabledNext : Next}/>
             </PagerButton>
         </PagerWrapper>)
 }
--- src/pager/styled.js
+++ src/pager/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 export const PagerWrapper = styled.div`
 display:flex;
-
 align-items:center;
 width:fit-content;
 margin:0 auto;
@@ -15,23 +14,37 @@ display:flex;
 flex-direction:row;
 align-items:center;
 justify-content:center;
+background-color:${({theme}) => theme.color.lightBlue};
 padding: 8px 16px;
 margin: 0 12px;
 outline:none;
 border:0;
 border-radius: 5px;
-background-color:${({theme}) => theme.color.grey};
 &:hover{
     cursor:pointer;
+}
+&:disabled{
+    background-color:${({theme}) => theme.color.grey};
+    color: ${({theme}) => theme.color.black}
+}
+@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
+    margin: 0 4px;
+    padding: 8px 12px;
 }
 `;
 export const PagerPrev = styled.img`
 width:7px;
 margin-right:8px;
+@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
+    margin: 0 4px;
+}
 `;
 export const PagerNext = styled.img`
 width:7px;
 margin-left:8px;
+@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
+    margin: 0 4px;
+}
 `;
 export const PagerText = styled.p`
 margin:0;
@@ -45,6 +58,9 @@ font-family: Poppins;
 color: ${({theme}) => theme.color.darkerGrey};
 font-size: 16px;
 line-height: 150%;
+@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
+    margin: 0 4px;
+}
 `;
 
 export const Bold = styled.b`
