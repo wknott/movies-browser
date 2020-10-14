7cabffb8ec1213035bb4c0eff19b6c396d098291
Wojciech Knott <wojciechknott@gmail.com>
2020-10-14 13:47:28 +0200

    fixed error with same key

--- src/features/people/PeopleContainer/index.js
+++ src/features/people/PeopleContainer/index.js
@@ -1,10 +1,11 @@
+import { nanoid } from "@reduxjs/toolkit";
 import React from "react";
 import PersonTile from "../PersonTile";
 import { StyledPeopleContainer } from "./styled";
 
 const PeopleContainer = ({ people }) => (
   <StyledPeopleContainer>
-    {people !== [] ? people.map(person => <PersonTile key={person.id} person={person} />)
+    {people !== [] ? people.map(person => <PersonTile key={nanoid()} person={person} />)
       : <h1>brak osób do pokazania</h1>}
   </StyledPeopleContainer>
 );
