5acbc35301a75b71c8b4b855be09cbf672d1f33f
Wojciech Knott <wojciechknott@gmail.com>
2020-10-15 18:12:56 +0200

    removed genres from movieList component

--- src/features/movies/MovieListPage/index.js
+++ src/features/movies/MovieListPage/index.js
@@ -2,11 +2,9 @@ import React, { useEffect } from "react";
 import Header from "../../../common/Header";
 import Wrapper from "../../../common/Wrapper";
 import { Pager } from "../../../common/Pager";
-import { getGenreName } from "../getGenreName";
 import { MoviesContainer } from "../MoviesContainer";
-import MovieTile from "../MovieTile";
 const { useDispatch, useSelector } = require("react-redux");
-const { selectMovies, selectLoading, selectGenres, selectSearchQuery, selectCurrentPage, fetchMovies } = require("../moviesSlice");
+const { selectMovies, selectLoading, selectSearchQuery, selectCurrentPage, fetchMovies } = require("../moviesSlice");
 
 const MovieListPage = () => {
   const dispatch = useDispatch();
@@ -18,34 +16,14 @@ const MovieListPage = () => {
 
   const loading = useSelector(selectLoading);
   const movies = useSelector(selectMovies);
-  const genres = useSelector(selectGenres);
   const searchQuery = useSelector(selectSearchQuery);
-
-  const mapGenreIds = (genreIds, genres) => {
-    return genreIds.map((genreId) => {
-      return getGenreName(genreId, genres);
-    });
-  };
-
-  const generateMovies = (movies) => {
-    return movies.map((movie) => {
-      const genreNames = mapGenreIds(movie.genre_ids, genres);
-      return (
-        <MovieTile
-          key={movie.id}
-          genreNames={genreNames}
-          movie={movie}
-        ></MovieTile>
-      );
-    });
-  };
 
   if (!loading && movies.length) {
     return (
       <div className="App">
         <Wrapper>
           <Header>Search results for "{searchQuery.query}"</Header>
-          <MoviesContainer>{generateMovies(movies)}</MoviesContainer>
+          <MoviesContainer movies={movies} />
           <Pager></Pager>
         </Wrapper>
       </div>
