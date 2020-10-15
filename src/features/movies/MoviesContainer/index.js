1b97075bb710ea591ad15f89b6ffbe9127294b1c
Wojciech Knott <wojciechknott@gmail.com>
2020-10-15 17:26:12 +0200

    moved movies generate to MoviesContainer

--- src/features/movies/MoviesContainer/index.js
+++ src/features/movies/MoviesContainer/index.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectGenres } from "../moviesSlice";
 import { StyledMoviesContainer } from "./styled";
+import MovieTile from "../MovieTile";
+import { getGenreName } from "../getGenreName";
 
-export const MoviesContainer = (props) => {
-  return <StyledMoviesContainer>{props.children}</StyledMoviesContainer>;
+export const MoviesContainer = ({ movies }) => {
+  const genres = useSelector(selectGenres);
+
+  const mapGenreIds = (genreIds, genres) => {
+    return genreIds.map((genreId) => {
+      return getGenreName(genreId, genres);
+    });
+  };
+
+  const generateMovies = (movies) => {
+    return movies.map((movie) => {
+      const genreNames = mapGenreIds(movie.genre_ids, genres);
+      return (
+        <MovieTile
+          key={movie.id}
+          genreNames={genreNames}
+          movie={movie}
+        />
+      );
+    });
+  };
+  return <StyledMoviesContainer>{generateMovies(movies)}</StyledMoviesContainer>;
 };
--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -8,55 +8,33 @@ import {
   selectLoading,
   selectMovies,
   fetchGenres,
-  selectGenres,
   selectCurrentPage
 } from "../moviesSlice";
-import MovieTile from "../MovieTile";
 import Header from "../../../common/Header";
-import { getGenreName } from "../getGenreName";
 import { Pager } from "../../../Pager";
 
 export default () => {
   const dispatch = useDispatch();
   const currentPage = useSelector(selectCurrentPage);
-  
-  useEffect( () => {
+
+  useEffect(() => {
     dispatch(fetchGenres());
-  },[dispatch]);
+  }, [dispatch]);
 
   useEffect(() => {
     dispatch(fetchPopularMovies(currentPage));
-  }, [dispatch,currentPage]);
+  }, [dispatch, currentPage]);
 
   const loading = useSelector(selectLoading);
   const movies = useSelector(selectMovies);
-  const genres = useSelector(selectGenres);
 
-  const mapGenreIds = (genreIds, genres) => {
-    return genreIds.map((genreId) => {
-      return getGenreName(genreId, genres);
-    });
-  };
-
-  const generateMovies = (movies) => {
-    return movies.map((movie) => {
-      const genreNames = mapGenreIds(movie.genre_ids, genres);
-      return (
-        <MovieTile
-          key={movie.id}
-          genreNames={genreNames}
-          movie={movie}
-        ></MovieTile>
-      );
-    });
-  };
 
   if (!loading && movies) {
     return (
       <div className="App">
         <Wrapper>
           <Header>Popular movies</Header>
-          <MoviesContainer>{generateMovies(movies)}</MoviesContainer>
+          <MoviesContainer movies={movies} />
           <Pager></Pager>
         </Wrapper>
       </div>
