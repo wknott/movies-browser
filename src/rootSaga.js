3efb9d9f00e7e04a8b64a1fdec271c0a016f6ab0
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-17 01:44:49 +0200

    changed name to watchFetchMovies in rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,10 @@
 import { all } from "redux-saga/effects";
 import { watchFetchPopularMovies } from "./features/movies/moviesSaga";
-import { watchFetchPopularPeople } from "./features/people/peopleSaga";
+import { watchFetchPeople } from "./features/people/peopleSaga";
 
 export default function* rootSaga() {
   yield all([
     watchFetchPopularMovies(),
-    watchFetchPopularPeople(),
+    watchFetchPeople(),
   ]);
 }
