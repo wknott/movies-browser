70550cfc8d9c0929905ce09c4eb328dbf33d9852
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-16 12:28:26 +0200

    xxx

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -1,31 +1,30 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const activeClassName = "link-active";
 
 export const Container = styled.div`
   width: 100%;
-  background-color: ${({ theme }) => theme.color.black};
-  display: flex;
-  justify-content: center;
-  padding: 10px;
-`;
-
-export const Wrapper = styled.div`
-  width: 100%;
-  max-width: 1320px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
+  background-color: ${({ theme }) => theme.color.woodsmoke};
 `;
 
 export const NavigationStyle = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 10px;
 `;
 
 export const Icon = styled.img`
-  height: 23px;
-  width: 30px;
-  margin: 0 10px;
+  height: 40px;
+  width: 40px;
+  margin-right: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 17px;
+    height: 17px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -35,24 +34,53 @@ export const Title = styled.h1`
   line-height: 40px;
   letter-spacing: -1.5px;
   display: flex;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 13px;
+    line-height: 130%;
+    letter-spacing: -0.5px;
+    margin: 0px;
+  }
 `;
 
 export const NavigationList = styled.ul`
   list-style: none;
   display: flex;
   justify-content: center;
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding: 0;
+  }
 `;
 
 export const NavigationItem = styled.li`
   border: 1px solid transparent;
   flex-grow: 0;
-  margin: 10px;
-  padding: 10px;
 `;
 
-export const Link = styled.a`
-  color: white;
-  font-size: 14px;
-  font-weight: 600;
+export const StyledLink = styled(NavLink).attrs(() => ({ activeClassName }))`
+  &.${activeClassName} {
+    border: 1px solid ${({ theme }) => theme.color.white};
+    border-radius: 24px;
+  }
+  
+  padding: 13.5px 24px;
+  color: ${({ theme }) => theme.color.white};
+  text-decoration: none;
   text-transform: uppercase;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 12px;
+    line-height: 18px;
+    padding: 8px 12px;
+
+    &.${activeClassName} {
+      border-radius: 29px;
+    }
+  }
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  align-items: center;
 `;
