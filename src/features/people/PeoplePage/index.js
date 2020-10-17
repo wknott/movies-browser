c8d3aa0096c3c4297254612b326f9bcc28100a38
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-17 02:36:38 +0200

    peoplePage interacts only with people state

--- src/features/people/PeoplePage/index.js
+++ src/features/people/PeoplePage/index.js
@@ -2,20 +2,19 @@ import React, { useEffect } from "react";
 import Wrapper from "../../../common/Wrapper";
 import Header from "../../../common/Header";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPeople, selectLoading, selectPeople } from "../peopleSlice";
+import { fetchPeople, selectCurrentPeoplePage, selectLoading, selectPeople } from "../peopleSlice";
 import PeopleContainer from "../PeopleContainer";
 import Loader from "../../../common/Loader";
 import { useQueryParameter } from "../../search/queryParameters";
 import searchQueryParamName from "../../searchQueryParamName";
 import { Pager } from "../../../common/Pager";
-import { selectCurrentPage } from "../../movies/moviesSlice";
 
 export default () => {
   const query = useQueryParameter(searchQueryParamName);
   const dispatch = useDispatch();
   const people = useSelector(selectPeople);
   const loading = useSelector(selectLoading);
-  const currentPage = useSelector(selectCurrentPage);
+  const currentPage = useSelector(selectCurrentPeoplePage);
 
   useEffect(() => {
     dispatch(fetchPeople({ page: currentPage, query }))
