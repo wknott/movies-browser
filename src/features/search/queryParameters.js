73160893d8b6345a9509808d5f71f861f26de189
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-18 22:51:14 +0200

    rebuilt useReplaceQueryParameter hook a bit so it can restart the url search params

--- src/features/search/queryParameters.js
+++ src/features/search/queryParameters.js
@@ -5,24 +5,23 @@ export const useQueryParameter = (searchQueryParamName) => {
   return (new URLSearchParams(location.search)).get(searchQueryParamName);
 }
 
-export const useReplaceQueryParameter = () => {
+export const useReplaceQueryParameter = (restartQueries = false) => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const history = useHistory();
 
-  return ({ key, value }, restartQueries) => {
+  return ({ key, value }) => {
     if (value) {
       searchParams.set(key, value);
     } else {
       searchParams.delete(key);
     }
-
-    if(restartQueries)
+    if(restartQueries && searchParams.get(key))
     {
-      history.push(`${location.pathname}?${key}=${searchParams.get(key).toString()}`)
+        history.push(`${location.pathname}?${key}=${searchParams.get(key).toString()}`)
     }
     else{
-      history.push(`${location.pathname}?${searchParams.toString()}`)
+      searchParams ? history.push(`${location.pathname}?${searchParams.toString()}`) : history.push(`${location.pathname}`);
     }
   }
 }
