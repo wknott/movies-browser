99421ede94f49a39c07756b5c02c95bd98dcb616
Wojciech Knott <wojciechknott@gmail.com>
2020-10-19 13:34:12 +0200

    formatted css in styled.js, added smaller font-size, modified mobile display version with css

--- src/common/Pager/index.js
+++ src/common/Pager/index.js
@@ -3,102 +3,88 @@ import Prev from "./images/Prev.svg"
 import DisabledPrev from "./images/DisabledPrev.svg"
 import Next from "./images/Next.svg"
 import DisabledNext from "./images/DisabledNext.svg"
-import { PagerButton, PagerNext, PagerPrev, PagerText, PagerInfo, PagerWrapper, Bold } from "./styled"
+import {
+  PagerButton,
+  PagerNext,
+  PagerPrev,
+  PagerText,
+  PagerInfo,
+  PagerWrapper,
+  Bold,
+  MobilePagerPrev,
+  MobilePagerNext
+} from "./styled"
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllMoviesPages, setMoviesPage } from "../../features/movies/moviesSlice";
-import { theme } from "../../theme";
 import { selectPeopleAllPages, setPeoplePage } from "../../features/people/peopleSlice";
 import { useLocation } from "react-router-dom";
 import { useQueryParameter, useReplaceQueryParameter } from "../../features/search/queryParameters";
 import pageParam from "../../features/pageQueryParamName";
 
-
 export const Pager = () => {
-    const mobileWidth = theme.breakpoint.mobileMax;
-    const location = useLocation();
-    const dispatch = useDispatch();
-    const atMovies = location.pathname.includes("movies") ? true : false;
-    const currentPage = +useQueryParameter(pageParam) || 1;
-    const allMoviesPages = useSelector(selectAllMoviesPages);
-    const allPeoplePages = useSelector(selectPeopleAllPages);
-    const allPages = atMovies ? allMoviesPages : allPeoplePages;
-    const replaceQueryParam = useReplaceQueryParameter();
-    const disableNext = currentPage === allPages;
-    const disablePrevious = currentPage === 1;
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const atMovies = location.pathname.includes("movies") ? true : false;
+  const currentPage = +useQueryParameter(pageParam) || 1;
+  const allMoviesPages = useSelector(selectAllMoviesPages);
+  const allPeoplePages = useSelector(selectPeopleAllPages);
+  const allPages = atMovies ? allMoviesPages : allPeoplePages;
+  const replaceQueryParam = useReplaceQueryParameter();
+  const disableNext = currentPage === allPages;
+  const disablePrevious = currentPage === 1;
 
-    useEffect(() => {
-        dispatch(atMovies ? setMoviesPage(currentPage) : setPeoplePage(currentPage));
-    },[currentPage,dispatch])
-    
-    const onFirstButtonClick = () => {
-        replaceQueryParam({key: pageParam, value: 1 })
-    }
-    const onPreviousButtonClick = () => {
-        replaceQueryParam({ key: pageParam, value: +currentPage - 1 });
-    }
-    const onNextButtonClick = () => {
-        replaceQueryParam({ key: pageParam, value: +currentPage + 1 });
-    }
-    const onLastButtonClick = () => {
-        replaceQueryParam({ key: pageParam, value: allPages });
-    }
+  useEffect(() => {
+    dispatch(atMovies ? setMoviesPage(currentPage) : setPeoplePage(currentPage));
+  }, [currentPage, dispatch, atMovies])
 
-    return (
-        <PagerWrapper>
-            <PagerButton
-                onClick={onFirstButtonClick}
-                disabled={disablePrevious}
-            >
-                <PagerPrev src={disablePrevious ? DisabledPrev : Prev} />
-                {window.innerWidth > mobileWidth ?
-                    <PagerText>
-                        First
-                </PagerText>
-                    : <PagerPrev src={disablePrevious ? DisabledPrev : Prev} />
-                }
+  const onFirstButtonClick = () => {
+    replaceQueryParam({ key: pageParam, value: 1 })
+  }
+  const onPreviousButtonClick = () => {
+    replaceQueryParam({ key: pageParam, value: +currentPage - 1 });
+  }
+  const onNextButtonClick = () => {
+    replaceQueryParam({ key: pageParam, value: +currentPage + 1 });
+  }
+  const onLastButtonClick = () => {
+    replaceQueryParam({ key: pageParam, value: allPages });
+  }
 
-            </PagerButton>
-
-            <PagerButton
-                onClick={onPreviousButtonClick}
-                disabled={disablePrevious}
-            >
-                <PagerPrev src={disablePrevious ? DisabledPrev : Prev} />
-                {window.innerWidth > mobileWidth ?
-                    <PagerText>
-                        Previous
-                </PagerText>
-                    : <></>}
-            </PagerButton>
-
-            <PagerInfo>
-                Page<Bold> {currentPage} </Bold>
-                of<Bold> {allPages} </Bold>
-            </PagerInfo>
-
-            <PagerButton
-                onClick={onNextButtonClick}
-                disabled={disableNext}
-            >
-                {window.innerWidth > mobileWidth ?
-                    <PagerText>
-                        Next
-                </PagerText>
-                    : <></>}
-                <PagerNext src={disableNext ? DisabledNext : Next} />
-            </PagerButton>
-
-            <PagerButton
-                onClick={onLastButtonClick}
-                disabled={disableNext}
-            >
-                {window.innerWidth > mobileWidth ?
-                    <PagerText>
-                        Last
-                </PagerText>
-                    : <PagerNext src={disableNext ? DisabledNext : Next} />
-                }
-                <PagerNext src={disableNext ? DisabledNext : Next} />
-            </PagerButton>
-        </PagerWrapper>)
+  return (
+    <PagerWrapper>
+      <PagerButton
+        onClick={onFirstButtonClick}
+        disabled={disablePrevious}
+      >
+        <PagerPrev src={disablePrevious ? DisabledPrev : Prev} />
+        <PagerText>First</PagerText>
+        <MobilePagerPrev src={disablePrevious ? DisabledPrev : Prev} />
+      </PagerButton>
+      <PagerButton
+        onClick={onPreviousButtonClick}
+        disabled={disablePrevious}
+      >
+        <PagerPrev src={disablePrevious ? DisabledPrev : Prev} />
+        <PagerText>Previous</PagerText>
+      </PagerButton>
+      <PagerInfo>
+        Page<Bold> {currentPage} </Bold>
+        of<Bold> {allPages} </Bold>
+      </PagerInfo>
+      <PagerButton
+        onClick={onNextButtonClick}
+        disabled={disableNext}
+      >
+        <PagerText>Next</PagerText>
+        <PagerNext src={disableNext ? DisabledNext : Next} />
+      </PagerButton>
+      <PagerButton
+        onClick={onLastButtonClick}
+        disabled={disableNext}
+      >
+        <PagerText>Last</PagerText>
+        <MobilePagerNext src={disableNext ? DisabledNext : Next} />
+        <PagerNext src={disableNext ? DisabledNext : Next} />
+      </PagerButton>
+    </PagerWrapper>)
 }
--- src/common/Pager/styled.js
+++ src/common/Pager/styled.js
@@ -1,68 +1,94 @@
 import styled from "styled-components"
 
 export const PagerWrapper = styled.div`
-display:flex;
-align-items:center;
-width:fit-content;
-margin:0 auto;
-margin-top:40px;
-margin-bottom: 100px;
+    display:flex;
+    align-items:center;
+    width:fit-content;
+    margin:0 auto;
+    margin-top:40px;
+    margin-bottom: 100px;
 `;
 
 export const PagerButton = styled.button`
-display:flex;
-flex-direction:row;
-align-items:center;
-justify-content:center;
-background-color:${({theme}) => theme.color.lightBlue};
-padding: 8px 16px;
-margin: 0 12px;
-outline:none;
-border:0;
-border-radius: 5px;
-&:hover{
-    cursor:pointer;
-}
-&:disabled{
-    background-color:${({theme}) => theme.color.grey};
-    color: ${({theme}) => theme.color.black}
-}
-@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
-    margin: 0 4px;
-    padding: 8px 12px;
-}
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+    background-color:${({ theme }) => theme.color.lightBlue};
+    padding: 8px 16px;
+    margin: 0 12px;
+    outline:none;
+    border:0;
+    border-radius: 5px;
+
+    &:hover{
+        cursor:pointer;
+    }
+
+    &:disabled{
+        background-color:${({ theme }) => theme.color.grey};
+        color: ${({ theme }) => theme.color.black}
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        margin: 0 4px;
+        padding: 8px 12px;
+    }
 `;
+
 export const PagerPrev = styled.img`
-width:7px;
-margin-right:8px;
-@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
-    margin: 0 4px;
-}
+    width:7px;
+    margin-right:8px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        margin: 0 4px;
+    }
 `;
+
+export const MobilePagerPrev = styled(PagerPrev)`
+    @media(min-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        display: none;
+    }
+`;
+
 export const PagerNext = styled.img`
-width:7px;
-margin-left:8px;
-@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
-    margin: 0 4px;
-}
+    width:7px;
+    margin-left:8px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        margin: 0 4px;
+    }
 `;
+
+export const MobilePagerNext = styled(PagerNext)`
+    @media(min-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        display: none;
+    }
+`;
+
 export const PagerText = styled.p`
-margin:0;
-font-family: Poppins;
-font-size: 14px;
-line-height: 140%;
+    margin:0;
+    font-family: Poppins;
+    font-size: 14px;
+    line-height: 140%;
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        display: none;
+    }
 `;
+
 export const PagerInfo = styled.p`
-margin:0 24px;
-font-family: Poppins;
-color: ${({theme}) => theme.color.darkerGrey};
-font-size: 16px;
-line-height: 150%;
-@media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px){
-    margin: 0 4px;
-}
+    margin:0 24px;
+    font-family: Poppins;
+    color: ${({ theme }) => theme.color.darkerGrey};
+    font-size: 16px;
+    line-height: 150%;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        margin: 0 4px;
+        font-size: 10px;
+    }
 `;
 
 export const Bold = styled.b`
-color:${({theme}) => theme.color.black};
+    color:${({ theme }) => theme.color.black};
 `;
