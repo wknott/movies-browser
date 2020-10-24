1d839781534b2f51fe8b0ebe5d2184ab66386762
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-10-24 14:33:21 +0200

    changed themeprovider location in app structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import { toMovie, toMovies, toPeople, toPerson } from "./routes";
+import { ThemeProvider } from "styled-components";
+import { theme, darkTheme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
 import MoviePage from "./features/movies/MoviePage";
 import MoviesPage from "./features/movies/MoviesPage";
 import PersonPage from "./features/people/PersonPage";
@@ -8,26 +11,33 @@ import PeoplePage from "./features/people/PeoplePage";
 import Navigation from "./common/Navigation/";
 import ScrollArrow from "./common/ScrollArrow/";
 
-export default () => (
+export default () => {
+    const isDark = true;
+    const currentTheme = isDark ? darkTheme : theme;
+
+  return (
   <HashRouter>
-    <Navigation />
-    <ScrollArrow />
-    <Switch>
-      <Route path={toMovie()}>
-        <MoviePage />
-      </Route>
-      <Route path={toMovies()}>
-        <MoviesPage />
-      </Route>
-      <Route path={toPerson()}>
-        <PersonPage />
-      </Route>
-      <Route path={toPeople()}>
-        <PeoplePage />
-      </Route>
-      <Route>
-        <Redirect to={toMovies()} />
-      </Route>
-    </Switch>
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyle />
+      <Navigation />
+      <ScrollArrow />
+      <Switch>
+        <Route path={toMovie()}>
+          <MoviePage />
+        </Route>
+        <Route path={toMovies()}>
+          <MoviesPage />
+        </Route>
+        <Route path={toPerson()}>
+          <PersonPage />
+        </Route>
+        <Route path={toPeople()}>
+          <PeoplePage />
+        </Route>
+        <Route>
+          <Redirect to={toMovies()} />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   </HashRouter>
-);
+);}
--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import { GlobalStyle } from "./GlobalStyle";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <App />
-      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
