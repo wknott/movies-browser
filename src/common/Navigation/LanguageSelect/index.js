2ecee4deaa1b4f1a4e62afa3090bf3d5669cd9ab
Wojciech Knott <wojciechknott@gmail.com>
2020-10-25 17:08:15 +0100

    added language select styles

--- src/common/Navigation/LanguageSelect/index.js
+++ src/common/Navigation/LanguageSelect/index.js
@@ -2,19 +2,20 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { languages } from "../../../languages";
 import { selectLanguage, setLanguage } from "./languageSlice";
+import { Select } from "./styled";
 
 const LanguageSelect = () => {
   const dispatch = useDispatch();
   const language = useSelector(selectLanguage);
 
   return (
-    <select value={language} onChange={({ target }) => dispatch(setLanguage(target.value))}>
+    <Select value={language} onChange={({ target }) => dispatch(setLanguage(target.value))}>
       {languages.map(language =>
         <option value={language.code} key={language.code}>
           {language.label}
         </option>
       )}
-    </select>
+    </Select>
   );
 }
 
