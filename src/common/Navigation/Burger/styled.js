9e741be2513ebc0cd9d04cebe917e72847fd7f8c
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-12-06 22:24:04 +0100

    added translateY to burger toggle animation

--- src/common/Navigation/Burger/styled.js
+++ src/common/Navigation/Burger/styled.js
@@ -11,11 +11,11 @@ export const StyledBurger = styled.button`
   cursor: pointer;
   padding: 0;
   z-index: 10;
-  
+
   &:focus {
     outline: none;
   }
-  
+
   div {
     width: 2rem;
     height: 0.25rem;
@@ -24,18 +24,20 @@ export const StyledBurger = styled.button`
     transition: all 0.3s linear;
     position: relative;
     transform-origin: 1px;
-    
+
     :first-child {
-      transform: ${({ open }) => open ? 'rotate(45deg)' : 'rotate(0)'};
+      transform: ${({ open }) =>
+        open ? "rotate(45deg) translateY(0.05rem)" : "rotate(0)"};
     }
 
     :nth-child(2) {
-      opacity: ${({ open }) => open ? '0' : '1'};
-      transform: ${({ open }) => open ? 'translateX(20px)' : 'translateX(0)'};
+      opacity: ${({ open }) => (open ? "0" : "1")};
+      transform: ${({ open }) => (open ? "translateX(20px)" : "translateX(0)")};
     }
 
     :nth-child(3) {
-      transform: ${({ open }) => open ? 'rotate(-45deg)' : 'rotate(0)'};
+      transform: ${({ open }) =>
+        open ? "rotate(-45deg) translateY(-0.05rem)" : "rotate(0)"};
     }
   }
 `;
