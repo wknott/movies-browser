dff34fc0ba2d8c2ed2ff514f26213d6e2b1fdfec
Wojciech Knott <wojciechknott@gmail.com>
2020-12-23 11:17:03 +0100

    added open sans font to styled link

--- src/common/Error/styled.js
+++ src/common/Error/styled.js
@@ -33,7 +33,7 @@ export const ReturnButton = styled.button`
   border: none;
   border-radius: 5px;
   padding: 16px 24px;
-  font-family: Open Sans;
+  font-family: "Open Sans", sans-serif;
   font-style: normal;
   font-weight: bold;
   font-size: 14px;
