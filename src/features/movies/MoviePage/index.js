f0ba996ce2e407436c76dced0b78fffe64e6610e
Wojciech Knott <wojciechknott@gmail.com>
2020-12-23 14:12:55 +0100

    removed history push from movie and person page

--- src/features/movies/MoviePage/index.js
+++ src/features/movies/MoviePage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchMovie, selectError, selectLoading, selectMovie } from "../moviesSlice";
 import {
   MovieBackdrop,
@@ -18,8 +18,6 @@ import MovieDetailsTile from "./MovieDetailsTile";
 import PeopleContainer from "../../people/PeopleContainer";
 import Header from "../../../common/Header";
 import Loader from "../../../common/Loader";
-import { useQueryParameter } from "../../search/queryParameters";
-import searchQueryParamName from "../../searchQueryParamName";
 import Error from "../../../common/Error/index";
 import { selectLanguage } from "../../../common/Navigation/LanguageSelect/languageSlice";
 import { cast, crew, votes } from "../../../languages";
@@ -29,10 +27,6 @@ export default () => {
   const movie = useSelector(selectMovie);
   const loading = useSelector(selectLoading);
   const dispatch = useDispatch();
-  const query = useQueryParameter(searchQueryParamName);
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const history = useHistory();
   const error = useSelector(selectError);
   const language = useSelector(selectLanguage);
 
@@ -42,17 +36,13 @@ export default () => {
     }
   }, [dispatch, id, language]);
 
-  useEffect(() => {
-    if (query) {
-      history.push(`/movies?${searchParams.toString()}`);
-    }
-  }, [query, history, searchParams]);
-
   if (error) {
-    return <Wrapper>
-      <Error />
-    </Wrapper>
-  }
+    return (
+      <Wrapper>
+        <Error />
+      </Wrapper>
+    );
+  };
 
   return (
     !loading && movie && movie.credits.cast ?
