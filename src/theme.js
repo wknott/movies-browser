5e63fbde06bc06763b1cb3a0be0092ef5ccfacad
Wojciech Knott <wojciechknott@gmail.com>
2020-12-23 12:58:24 +0100

    moved breakpoint outside theme

--- src/theme.js
+++ src/theme.js
@@ -1,96 +1,93 @@
+const breakpoint = {
+  miniMobile: 575.98,
+  mobileMax: 767.98,
+  ipad: 991.98,
+  hdDesktop: 1199,
+};
+
 export const theme = {
-    navBar: {
-        color: {
-            black: "#000000",
-            white: "#FFFFFF",
-        }
-    },
-    movieBanner: {
-        color: {
-            black: "#000000",
-            white: "#FFFFFF",
-        }
-    },
+  navBar: {
     color: {
-        black: "#000000",
-        woodsmoke: "#18181B",
-        blue: "#0044CC",
-        silver: "#C4C4C4",
-        lightBlue: "#D6E4FF",
-        yellow: "#FCD420",
-        stormGray: "#74788B",
-        darkerGrey: "#7E839A",
-        grey: "#E4E6F0",
-        mercury: "#E5E5E5",
-        whisper: "#F5F5FA",
-        white: "#FFFFFF",
-    },
-    basicColor: {
-        black: "#000000",
-        woodsmoke: "#18181B",
-        blue: "#0044CC",
-        silver: "#C4C4C4",
-        lightBlue: "#D6E4FF",
-        yellow: "#FCD420",
-        stormGray: "#74788B",
-        darkerGrey: "#7E839A",
-        grey: "#E4E6F0",
-        mercury: "#E5E5E5",
-        whisper: "#F5F5FA",
-        white: "#FFFFFF",
-    },
-    breakpoint: {
-        miniMobile: 568,
-        mobileMax: 767,
-        ipad: 1040,
-        hdDesktop: 1366,
-    },
+      black: "#000000",
+      white: "#FFFFFF",
+    }
+  },
+  movieBanner: {
+    color: {
+      black: "#000000",
+      white: "#FFFFFF",
+    }
+  },
+  color: {
+    black: "#000000",
+    woodsmoke: "#18181B",
+    blue: "#0044CC",
+    silver: "#C4C4C4",
+    lightBlue: "#D6E4FF",
+    yellow: "#FCD420",
+    stormGray: "#74788B",
+    darkerGrey: "#7E839A",
+    grey: "#E4E6F0",
+    mercury: "#E5E5E5",
+    whisper: "#F5F5FA",
+    white: "#FFFFFF",
+  },
+  basicColor: {
+    black: "#000000",
+    woodsmoke: "#18181B",
+    blue: "#0044CC",
+    silver: "#C4C4C4",
+    lightBlue: "#D6E4FF",
+    yellow: "#FCD420",
+    stormGray: "#74788B",
+    darkerGrey: "#7E839A",
+    grey: "#E4E6F0",
+    mercury: "#E5E5E5",
+    whisper: "#F5F5FA",
+    white: "#FFFFFF",
+  },
+  breakpoint
 };
 export const darkTheme = {
-    navBar: {
-        color: {
-            black: "#EEEEEE",
-            white: "#FFFFFF",
-        }
-    },
-    movieBanner: {
-        color: {
-            black: "#EEEEEE",
-            white: "#000000",
-        }
-    },
+  navBar: {
     color: {
-        black: "#EEEEEE",
-        woodsmoke: "#E7E7E4",
-        blue: "#ffbb33",
-        silver: "#3b3b3b",
-        lightBlue: "#291b00",
-        yellow: "#032bdf",
-        stormGray: "#8b8774",
-        darkerGrey: "#817c65",
-        grey: "#1b190f",
-        mercury: "#1a1a1a",
-        whisper: "#0a0a05",
-        white: "#000000",
-    },
-    basicColor: {
-        black: "#000000",
-        woodsmoke: "#18181B",
-        blue: "#0044CC",
-        silver: "#C4C4C4",
-        lightBlue: "#D6E4FF",
-        yellow: "#FCD420",
-        stormGray: "#74788B",
-        darkerGrey: "#7E839A",
-        grey: "#E4E6F0",
-        mercury: "#E5E5E5",
-        whisper: "#F5F5FA",
-        white: "#FFFFFF",
-    },
-    breakpoint: {
-        miniMobile: 575.98,
-        mobileMax: 767.98,
-        ipad: 991.98,
-        hdDesktop: 1199,
-    },
+      black: "#EEEEEE",
+      white: "#FFFFFF",
+    }
+  },
+  movieBanner: {
+    color: {
+      black: "#EEEEEE",
+      white: "#000000",
+    }
+  },
+  color: {
+    black: "#EEEEEE",
+    woodsmoke: "#E7E7E4",
+    blue: "#ffbb33",
+    silver: "#3b3b3b",
+    lightBlue: "#291b00",
+    yellow: "#032bdf",
+    stormGray: "#8b8774",
+    darkerGrey: "#817c65",
+    grey: "#1b190f",
+    mercury: "#1a1a1a",
+    whisper: "#0a0a05",
+    white: "#000000",
+  },
+  basicColor: {
+    black: "#000000",
+    woodsmoke: "#18181B",
+    blue: "#0044CC",
+    silver: "#C4C4C4",
+    lightBlue: "#D6E4FF",
+    yellow: "#FCD420",
+    stormGray: "#74788B",
+    darkerGrey: "#7E839A",
+    grey: "#E4E6F0",
+    mercury: "#E5E5E5",
+    whisper: "#F5F5FA",
+    white: "#FFFFFF",
+  },
+  breakpoint
 };
