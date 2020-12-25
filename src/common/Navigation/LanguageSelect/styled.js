c6d5b5ce2d90e06fccf40a88a5687d8afea36cdc
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2020-12-25 14:13:55 +0100

    fixed issues with wrong colors on theme toggle

--- src/common/Navigation/LanguageSelect/styled.js
+++ src/common/Navigation/LanguageSelect/styled.js
@@ -4,7 +4,7 @@ export const Select = styled.select`
   height: 50px;
   width: 100%;
   padding: 16px 24px 16px 8px;
-  background-color: none;
+  background-color: ${({ theme }) => theme.basicColor.white};
   border: none;
   outline: none;
   font-size: 16px;
@@ -14,21 +14,23 @@ export const Select = styled.select`
 export const SelectContainer = styled.div`
   height: 50px;
   padding: 0 24px;
-  background-color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.basicColor.white};
   align-items: center;
   border-radius: 33px;
   align-self: center;
 
-  ${({ mobile }) => mobile && css`
-    @media (min-width: ${({ theme }) => theme.breakpoint.ipad}px) {
-      display: none;
-    };
-  `}
-  
+  ${({ mobile }) =>
+    mobile &&
+    css`
+      @media (min-width: ${({ theme }) => theme.breakpoint.ipad}px) {
+        display: none;
+      } ;
+    `}
+
   @media (max-width: ${({ theme }) => theme.breakpoint.ipad}px) {
     margin-left: 0;
     margin-top: 10px;
     width: 100%;
     min-width: 200px;
-  };
+  } ;
 `;
--- src/features/movies/MoviePage/styled.js
+++ src/features/movies/MoviePage/styled.js
@@ -1,32 +1,67 @@
 const { default: styled } = require("styled-components");
 
 export const MovieBackdrop = styled.div`
-  background-color: ${({ theme }) => theme.movieBanner.color.black};
+  background-color: ${({ theme }) => theme.basicColor.black};
   color: ${({ theme }) => theme.movieBanner.color.white};
-  background-image: 
-  linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%), 
-  linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%), 
-  linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%), 
-  linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%), 
-  url(${({ src }) => src});
-  background-size: 1920px 770px, 1920px 770px, 1920px 770px, 1920px 770px, 1368px 769px;
+  background-image: linear-gradient(
+      270deg,
+      #000000 14.11%,
+      rgba(0, 0, 0, 0.873268) 15.08%,
+      rgba(0, 0, 0, 0.720529) 16.51%,
+      rgba(0, 0, 0, 0.294577) 19.99%,
+      rgba(0, 0, 0, 0.159921) 21.88%,
+      rgba(0, 0, 0, 0) 25.68%
+    ),
+    linear-gradient(
+      90deg,
+      #000000 13.6%,
+      rgba(0, 0, 0, 0.984059) 14.58%,
+      rgba(0, 0, 0, 0.967732) 15.44%,
+      rgba(0, 0, 0, 0.865569) 16.3%,
+      rgba(0, 0, 0, 0.230315) 22.87%,
+      rgba(0, 0, 0, 0) 26.64%
+    ),
+    linear-gradient(
+      180deg,
+      #000000 0%,
+      rgba(0, 0, 0, 0.689555) 4.66%,
+      rgba(0, 0, 0, 0.439033) 9.34%,
+      rgba(0, 0, 0, 0.20628) 15.16%,
+      rgba(0, 0, 0, 0) 24.22%
+    ),
+    linear-gradient(
+      189.44deg,
+      rgba(0, 0, 0, 0) 58.48%,
+      rgba(0, 0, 0, 0.106473) 63.17%,
+      rgba(0, 0, 0, 0.235359) 68.85%,
+      rgba(0, 0, 0, 0.492821) 78.08%,
+      rgba(0, 0, 0, 0.740286) 85.86%,
+      #000000 92.87%
+    ),
+    url(${({ src }) => src});
+  background-size: 1920px 770px, 1920px 770px, 1920px 770px, 1920px 770px,
+    1368px 769px;
   background-repeat: no-repeat;
   background-position: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.hdDesktop}px) {
-    background-size: 1366px 525px, 1366px 525px, 1366px 525px, 1366px 525px, 1024px 525px;
+    background-size: 1366px 525px, 1366px 525px, 1366px 525px, 1366px 525px,
+      1024px 525px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.ipad}px) {
-    background-size: 1040px 400px, 1040px 400px, 1040px 400px, 1040px 400px, 776px 400px;
+    background-size: 1040px 400px, 1040px 400px, 1040px 400px, 1040px 400px,
+      776px 400px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    background-size: 700px 300px, 700px 300px, 700px 300px, 700px 300px, 550px 300px
+    background-size: 700px 300px, 700px 300px, 700px 300px, 700px 300px,
+      550px 300px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoint.miniMobile}px) {
-    background-size: 450px 188px, 450px 188px, 450px 188px, 450px 188px, 350px 188px;
+    background-size: 450px 188px, 450px 188px, 450px 188px, 450px 188px,
+      350px 188px;
   }
 `;
 
@@ -92,7 +127,7 @@ export const MovieRatingImg = styled.img`
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     height: 16px;
-  };
+  } ;
 `;
 
 export const MovieRatingNote = styled.p`
@@ -106,7 +141,7 @@ export const MovieRatingNote = styled.p`
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     font-size: 14px;
     font-weight: 600;
-  };
+  } ;
 `;
 
 export const MovieRatingText = styled.p`
@@ -122,7 +157,7 @@ export const MovieRatingText = styled.p`
     margin-top: 2px;
     margin-right: 8px;
     flex-basis: unset;
-  };
+  } ;
 `;
 
 export const MovieRatingTextVote = styled(MovieRatingText)`
@@ -130,5 +165,5 @@ export const MovieRatingTextVote = styled(MovieRatingText)`
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     margin-top: 2px;
-  };
-`; 
+  } ;
+`;
