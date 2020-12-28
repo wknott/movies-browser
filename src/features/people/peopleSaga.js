e3c5b0f11132b786dd53d7a01e8fbb23504e4840
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2020-12-28 22:03:57 +0100

    removed fetchgenres from movies and people sagas

--- src/features/people/peopleSaga.js
+++ src/features/people/peopleSaga.js
@@ -8,44 +8,35 @@ import {
   fetchPeopleError,
   fetchPeopleSuccess,
 } from "./peopleSlice";
-import { getGenres } from "../movies/api";
-import { getGenreName } from "../movies/getGenreName";
 
 function* fetchPeopleHandler({ payload }) {
   try {
     const people = yield call(getPeople, payload);
-    yield put(fetchPeopleSuccess(
-      {
+    yield put(
+      fetchPeopleSuccess({
         people: people.results,
         allPages: people.total_pages,
         totalResults: people.total_results,
-      }
-    ));
+      })
+    );
   } catch (error) {
     yield put(fetchPeopleError());
   }
-};
+}
 
 function* fetchPersonHandler({ payload }) {
   try {
     const person = yield call(getPersonDetails, payload);
     const credits = yield call(getPersonMovieCredits, payload);
-    const genres = yield call(getGenres, { language: payload.language });
-    const crew = yield credits.crew.map(movie => {
-      const genresNames = movie.genre_ids.map(genre => getGenreName(genre, genres));
-      return { ...movie, genres: genresNames }
-    });
-    const cast = yield credits.cast.map(movie => {
-      const genresNames = movie.genre_ids.map(genre => getGenreName(genre, genres));
-      return { ...movie, genres: genresNames }
-    });
+    const crew = yield credits.crew;
+    const cast = yield credits.cast;
     yield put(fetchPersonSuccess({ ...person, cast, crew }));
   } catch (error) {
     yield put(fetchPersonError());
   }
-};
+}
 
 export function* watchFetchPeople() {
   yield takeLatest(fetchPeople.type, fetchPeopleHandler);
   yield takeLatest(fetchPerson.type, fetchPersonHandler);
-};
+}
