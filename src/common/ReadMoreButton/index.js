d63796704368d7cd3d32b6a7650e08630bfe47c8
Szymon Marczuk <56971694+Monsy99@users.noreply.github.com>
2020-12-28 22:12:58 +0100

    readMoreButtom default export fixed

--- src/common/ReadMoreButton/index.js
+++ src/common/ReadMoreButton/index.js
@@ -4,21 +4,19 @@ import { Button } from "./styled";
 import { selectLanguage } from "../../common/Navigation/LanguageSelect/languageSlice";
 import { readLess, readMore } from "../../common/languages";
 
-export default ({ content }) => {
+const ReadMoreButton = ({ content }) => {
   const [show, setShow] = useState(false);
   const language = useSelector(selectLanguage);
 
-  return (
-    content.length > 400 ?
-      <>
-        {show ? content : `${content.slice(0, 400)}...`}
-        <Button onClick={() => setShow(show => !show)}>
-          {show ? readLess[language] : readMore[language]}
-        </Button>
-      </>
-      :
-      <>
-        {content}
-      </>
-  )
+  return content.length > 400 ? (
+    <>
+      {show ? content : `${content.slice(0, 400)}...`}
+      <Button onClick={() => setShow((show) => !show)}>
+        {show ? readLess[language] : readMore[language]}
+      </Button>
+    </>
+  ) : (
+    <>{content}</>
+  );
 };
+export default ReadMoreButton;
